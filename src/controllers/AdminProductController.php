c5a4be89893504949505e3cc16e512b93466f8e0
killer-of-queen <killer0f0ueen@gmail.com>
2020-04-07 11:28:40 +0700

    added add product

--- src/controllers/AdminProductController.php
+++ src/controllers/AdminProductController.php
@@ -18,29 +18,29 @@ class AdminProductController extends AdminBase
 
         if (isset($_POST['submit'])) {
             $options['name'] = $_POST['name'];
-            $options['price'] = $_POST['price'];
+            $options['price'] = ($_POST['price'] == '') ? '0' : $_POST['price'];
             $options['short_name'] = $_POST['short_name'];
             $options['short_description'] = $_POST['short_description'];
             $options['description'] = $_POST['description'];
             $options['amount'] = $_POST['amount'];
-        }
 
-        $errors = false;
-        $success = false;
-        if (!isset($options['name']) || empty($options['name'])) {
-            $errors[] = 'Заполните поля';
-        }
+            $errors = false;
+            $success = false;
+            if (!isset($options['name']) || empty($options['name'])) {
+                $errors[] = 'Заполните поля';
+            }
 
-        if ($errors == false) {
-            $id = Product::createProduct($options);
-
-            if($id) {
-                $success = "Новый товар №" . $id . " был успешно добавлен";
-                require_once (ROOT . '/views/admin/success.php');
-                return true;
-                //if (is_uploaded_file($_FILES['image']['tmp_name'])) {
+            if ($errors == false) {
+                $id = Product::createProduct($options);
+                if($id) {
+                    $success = "Новый товар №" . $id . " был успешно добавлен";
+                    require_once (ROOT . '/views/admin/success.php');
+                    return true;
+                    //if (is_uploaded_file($_FILES['image']['tmp_name'])) {
                     //move_uploaded_file($_FILES['image']['tmp_name'], $_SERVER['DOCUMENT_ROOT'] .  '/template/images/');
-               // }
+                    // }
+                    return true;
+                }
             }
         }
 
